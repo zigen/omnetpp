9d29e7bdac422ad73ed16f469a92d6ed94aaf203
zigen <hrlclb@gmail.com>
2021-07-07 03:17:27 +0900

    temp

---
+++ samples/tictoc/post.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+arguments_.push("-u")
+arguments_.push("Qtenv")
+// arguments_.push("Cmdenv")
+arguments_.push("-m")
+arguments_.push("-c")
+arguments_.push("Tictoc3")
+// arguments_.push("tictoc18.ned")
+//Module.canvas.height=1200
+//Module.canvas.width=1200
